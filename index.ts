2ce6f6d1e25c7eafba7ff7aaff4983a2421d48e2
ngdangkhanhdev <ngdangkhanh233@gmail.com>
2024-07-21 16:20:05 +0700

    Nhúng file tĩnh

--- index.ts
+++ index.ts
@@ -12,6 +12,8 @@ const port: string = `${process.env.PORT}`;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+app.use(express.static("public"));
+
 clientRoutes(app);
 
 app.listen(port, () => {
