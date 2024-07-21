b252c7cc05ef0efcd8d96ef91958bf8faeb16718
ngdangkhanhdev <ngdangkhanh233@gmail.com>
2024-07-21 17:12:17 +0700

    Làm trang Danh mục tour (bên client) - Hiển thị data ra giao diện

--- controllers/client/category.controller.ts
+++ controllers/client/category.controller.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
+import Category from "../../models/category.model";
 
 // [GET] /categories
 export const index = async (req: Request, res: Response) => {
+    const categories = await Category.findAll({
+        where: {
+            deleted: false,
+            status: "active",
+        },
+        raw: true,
+    });
+
     res.render("client/pages/categories/index.pug", {
         pageTitle: "Danh mục tour",
+        categories: categories,
     });
 };
