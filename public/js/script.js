7f898ec81b7400198ee23d7f4488429846985399
ngdangkhanhdev <ngdangkhanh233@gmail.com>
2024-07-23 15:16:20 +0700

    Tính năng giỏ hàng và thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,43 @@ if (sliderMain) {
         },
     });
 }
+
+/* Cart */
+
+// Initial Cart
+const cart = localStorage.getItem("cart");
+if (!cart) {
+    localStorage.setItem("cart", JSON.stringify([]));
+}
+
+// Add Tour into Cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+    formAddToCart.addEventListener("submit", (e) => {
+        e.preventDefault;
+
+        const quantity = parseInt(formAddToCart.quantity.value);
+        const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+        if (quantity > 0 && tourId) {
+            const cart = JSON.parse(localStorage.getItem("cart"));
+
+            const indexExistTour = cart.findIndex(
+                (item) => item.tourId == tourId
+            );
+
+            if (indexExistTour == -1) {
+                cart.push({
+                    tourId: tourId,
+                    quantity: quantity,
+                });
+            } else {
+                cart[indexExistTour].quantity =
+                    cart[indexExistTour].quantity + quantity;
+            }
+            localStorage.setItem("cart", JSON.stringify(cart));
+        }
+    });
+}
+
+/* End Cart */
